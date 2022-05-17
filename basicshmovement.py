d20d566c5cf89df877694a134140cbe20133ee2f
banana <105305001+bananber@users.noreply.github.com>
2022-05-17 08:08:22 -0700

    Update basicshmovement.py

--- basicshmovement.py
+++ basicshmovement.py
@@ -6,15 +6,15 @@ babner.connect()
 print(babner.get_battery())
 # take off at 170cm
 babner.takeoff()
-babner.moveup(90)
+babner.moveup(120)
 # move forward 150cm
-babner.move_forward(150)
+babner.move_forward(310)
 sleep(1)
 # turn
 babner.rotate_clockwise(90)
 sleep(1)
 # move forward 50cm
-babner.move_forward(350)
+babner.move_forward(600)
 sleep(1)
 # turn
 babner.rotate_clockwise(90)
@@ -23,13 +23,13 @@ sleep(1)
 babner.flip()
 sleep(1)
 # move forward 150cm
-babner.move_forward(150)
+babner.move_forward(310)
 sleep(1)
 # turn
 babner.rotate_clockwise(90)
 sleep(1)
 # move forward
-babner.move_forward(350)
+babner.move_forward(600)
 sleep(1)
 # landing
 babner.land()
