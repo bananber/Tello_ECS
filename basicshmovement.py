ce32979ba3763bd9840aa75c116ebac8a959df14
banana <105305001+bananber@users.noreply.github.com>
2022-05-17 08:48:04 -0700

    Update basicshmovement.py

--- basicshmovement.py
+++ basicshmovement.py
@@ -4,32 +4,37 @@ from time import sleep
 babner = tello.Tello()
 babner.connect()
 print(babner.get_battery())
-# take off at 170cm
+# must be facing leftside wall
+# take off at 200cm
 babner.takeoff()
 babner.moveup(120)
-# move forward 150cm
+# move forward 310cm
 babner.move_forward(310)
 sleep(1)
 # turn
 babner.rotate_clockwise(90)
 sleep(1)
-# move forward 50cm
-babner.move_forward(600)
+# move forward 800cm
+babner.move_forward(500)
+sleep(1)
+babner.move_forward(300)
 sleep(1)
 # turn
 babner.rotate_clockwise(90)
 sleep(1)
 # cool slip
-babner.flip()
+babner.flip_forward()
 sleep(1)
-# move forward 150cm
+# move forward 310cm
 babner.move_forward(310)
 sleep(1)
 # turn
 babner.rotate_clockwise(90)
 sleep(1)
-# move forward
-babner.move_forward(600)
+# move forward 800cm
+babner.move_forward(500)
+sleep(1)
+babner.move_forward(300)
 sleep(1)
 # landing
 babner.land()
